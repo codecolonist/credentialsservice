142e4eb428b17df4198e9a512ce0584bd5fdeb3b
Ramkishorerao Gujjeti <codecolonist@gmail.com>
2019-04-24 23:24:55 -0500

    added eureka client

--- src/main/java/com/codecolonist/credentialsservice/CredentialsserviceApplication.java
+++ src/main/java/com/codecolonist/credentialsservice/CredentialsserviceApplication.java
@@ -2,8 +2,10 @@ package com.codecolonist.credentialsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CredentialsserviceApplication {
 
 	public static void main(String[] args) {
